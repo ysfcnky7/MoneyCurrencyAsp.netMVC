5f26497039a3ac2b4b2f8d5d60ca406acff361f8
Yusuf Çankaya <ysfcnky7@gmail.com>
2018-01-16 16:27:00 +0300

    WEB
    SunWeb uygulamasında uygulamayı çalıştırdığınız takdirde eğer
    veritabanında herhangi bir değer yok ise tablo boş olarak dönecektir.
    Bundan dolayı tavsiye edilen “Konsol” uygulamasını çalıştırıp değerlerin
    veritabanına kayıt olmasını sağlamaktır. Web uygulaması veritabanında
    kayıt olmuş verileri güncelleme yapabilmektedir.Tabloda istenen 12 adet
    döviz kuru için filtreleme yapılabilmektedir. Web projesinde
    EntityFramework’ün Database Model Seçeneği ile sisteme veritabanı
    entegre edilmiştir.

---
+++ SanWeb/SanWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using SanWeb.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SanWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        SANEntities db = new SANEntities();
+        public ActionResult Index()
+        {
+            return View(db.MoneyCurrencies.ToList());
+        }
+
+        [HttpPost]
+        public JsonResult SaveAndUpdateCurrency(int itemID, string idName, string idCode, string idBuying, string idSelling, string idReception, string idSales)
+        {
+            var result = new jsonMessage();
+            try
+            {
+                MoneyCurrency currency = new MoneyCurrency();
+                currency.Id = itemID;
+                currency.Name = idName;
+                currency.Code = idCode;
+                currency.Exchange_Buying = idBuying;
+                currency.Exchange_Selling = idSelling;
+                currency.Effective_Reception = idReception;
+                currency.Effective_Sales = idSales;
+                currency.Date = DateTime.Now;
+                db.Entry(currency).State = EntityState.Modified;
+                result.Message = "Başarıyla Güncelleştirildi...";
+                result.Status = true;
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                result.Status = false;
+                result.Message = "İşlem Başarısız Tekrar Deneyiniz";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ SanWeb/SanWeb/Models/jsonMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SanWeb.Models
+{
+    public class jsonMessage
+    {
+        public bool Status { get; set; }
+        public String Message { get; set; }
+    }
+}
